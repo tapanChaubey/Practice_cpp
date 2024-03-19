fc4d8681f140d0ad1e8b99da71d97c6e602a3051
Tapan Kumar <tapankumar08521@gmail.com>
2024-03-19 11:11:55 +0530

    change kth level itretively

--- BianryTree/kthlevel.cpp
+++ BianryTree/kthlevel.cpp
@@ -36,17 +36,47 @@ class Bainry{
     printInordered(root->left);
     printInordered(root->right);
  }
-void kthlevel(Node * root,int level, int k){
-    if(root==NULL){
+ void kthlevel(Node * root, int k){
+     if(root==NULL){
         return ;
     }
-    if(level==k){
-        cout<<root->data<<"-";
-       return;
+//     if(level==k){
+//         cout<<root->data<<"-";
+//        return;
+//     }
+
+// kthlevel(root->left,level+1,k);
+    // kthlevel(root->right,level+1,k);
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+    int l=1;
+    while(!q.empty()){
+        Node* curr=q.front();
+        q.pop();
+        if(curr==NULL){
+            if(q.empty()){
+               break;
+            }
+            else{
+                l++;
+                q.push(NULL);
+            }
+        }
+        else{
+            if(l==k){
+                cout<<curr->data<<"-";
+            }
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+        }
     }
 
-    kthlevel(root->left,level+1,k);
-    kthlevel(root->right,level+1,k);
+
 
 }
  
@@ -58,7 +88,7 @@ int main(){
     Bainry bt;
     bt.root=bt.bulit();
     //bt.printInordered(bt.root);
-    bt.kthlevel(bt.root,1,3);
+    bt.kthlevel(bt.root,3);
 
 
 }
